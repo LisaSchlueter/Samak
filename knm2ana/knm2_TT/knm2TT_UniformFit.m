fc2b19794d27b1cf13511d16a469e0a0f535e849
Thierry Lasserre <thierry.lasserre@cea.fr>
2020-04-01 12:25:59 +0200

    RunAnalysis: PlotResidualsMultiRing: Plot Fit Residuals for All rings and the MTD. Adapted from PlotFit

--- knm2ana/knm2_TT/knm2TT_UniformFit.m
+++ knm2ana/knm2_TT/knm2TT_UniformFit.m
@@ -13,8 +13,8 @@ else
     fprintf('start fit\n');
     RunAnaArg = {'RunList','KNM2_Prompt',... % all KNM2 golden runs
         'fixPar','mNu E0 Bkg Norm',...       % free Parameter !!
-        'DataType','Real',...
-        'FSDFlag','Sibille0p5eV',...         % final state distribution (theoretical calculation) 
+        'DataType','Twin',...
+        'FSDFlag','BlindingKNM2',...         % final state distribution (theoretical calculation) 
         'ELossFlag','KatrinT2',...           % energy loss function     ( different parametrizations available)
         'AnaFlag','StackPixel',...           % FPD segmentations -> pixel combination
         'chi2',chi2,...                      % statistics only
