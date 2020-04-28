586672c2ba744e0376c543db2e95d5998c3b2d38
Lisa Schlüter <lisa.schlueter@tum.de>
2020-04-28 23:16:48 +0200

    test parallel sterileII

--- knm1ana/knm1_UniformFit/knm1_UniformFit.m
+++ knm1ana/knm1_UniformFit/knm1_UniformFit.m
@@ -6,7 +6,7 @@ range = 40; % eV below the endpoint
 %% create MultiRunAnalysis object
 
 R = MultiRunAnalysis('RunList','KNM1',... % runlist defines which runs are analysed -> set MultiRunAnalysis.m -> function: GetRunList()
-    'chi2','chi2CMShape',...                 % uncertainties: statistical or stat + systematic uncertainties
+    'chi2','chi2Stat',...                 % uncertainties: statistical or stat + systematic uncertainties
     'DataType','Real',...                 % can be 'Real' or 'Twin' -> Monte Carlo
     'fixPar','mNu E0 Norm Bkg',...        % free Parameter!!
     'RadiativeFlag','ON',...              % theoretical radiative corrections applied in model
@@ -14,12 +14,12 @@ R = MultiRunAnalysis('RunList','KNM1',... % runlist defines which runs are analy
     'minuitOpt','min ; minos',...         % technical fitting options (minuit)
     'FSDFlag','Sibille0p5eV',...          % final state distribution
     'ELossFlag','KatrinT2',...            % energy loss function
-    'SysBudget',22,...
+    'SysBudget',22,...                    % defines syst. uncertainties -> in GetSysErr.m;
     'DopplerEffectFlag','OFF',...
     'Twin_SameCDFlag','ON',...
     'Twin_SameIsotopFlag','ON',...
-    'SynchrotronFlag','OFF',...
-    'AngularTFFlag','OFF');          % defines syst. uncertainties -> in GetSysErr.m;
+    'SynchrotronFlag','ON',...
+    'AngularTFFlag','OFF');        
 
 % R.ModelObj.mnu4Sq_i=200;
 R.exclDataStart = R.GetexclDataStart(range); % set region of interest
--- ksn1ana/LisaSterile/SterileTestPar.m
+++ ksn1ana/LisaSterile/SterileTestPar.m
@@ -9,9 +9,9 @@ chi2 = 'chi2Stat';
 DataType = 'Real';
 freePar = 'E0 Bkg Norm';
 RunList = 'KNM1';
-savefile = sprintf('%sSterileTest_%s_%s_%s_%.0feVrange_%.0fGridSteps.mat',...
+savefile = sprintf('%sSterileTestPar_%s_%s_%s_%.0feVrange_%.0fGridSteps.mat',...
     savedir,RunList,DataType,strrep(freePar,' ',''),range,nGridSteps);
-if exist(savefile,'file') 
+if exist(savefile,'file')
 else
     RunAnaArg = {'RunList',RunList,...
         'fixPar',freePar,...
@@ -25,9 +25,7 @@ else
         'AngularTFFlag','OFF',...
         'ISCSFlag','Edep',...
         'NonPoissonScaleFactor',NonPoissonScaleFactor};
-    tic;
     T = MultiRunAnalysis(RunAnaArg{:});
-    toc;
     T.exclDataStart = T.GetexclDataStart(range);
     
     T.Fit;
@@ -36,27 +34,42 @@ else
     %% define grid
     mnu4Sq = repmat(linspace(0,50^2,nGridSteps)',1,nGridSteps);
     sin2T4 = repmat(linspace(0.001,1,nGridSteps),nGridSteps,1); %logspace(-3,0,nGridSteps)
- 
+    
+    mnu4Sq_Grid = reshape(mnu4Sq',nGridSteps.*nGridSteps,1);
+    sin2T4_Grid = reshape(sin2T4',nGridSteps.*nGridSteps,1); 
     % make copy of models for parallel computing
-    D = copy(repmat(T,nGridSteps,nGridSteps));
+    D = copy(repmat(T,nGridSteps.*nGridSteps,1));
    % D = gpuArray(D);
-    arrayfun(@(x) x.SimulateStackRuns,D'); % get model object
-    arrayfun(@(x,mu,sin) x.ModelObj.SetFitBiasSterile(mu,sin),D',mnu4Sq,sin2T4); % set sterile parameters
-    
-    %%  start timed grid search
-    tic;
-    arrayfun(@(x) x.Fit,D)
-    toc;
-    
+    %arrayfun(@(x) x.SimulateStackRuns,D'); % get model object
+   % arrayfun(@(x,mu,sin) x.ModelObj.SetFitBiasSterile(mu,sin),D',mnu4Sq,sin2T4); % set sterile parameters
+   
+  %  arrayfun(@(x,mu,sin) x.ModelObj.SetFitBiasSterile(mu,sin),D,mnu4Sq_Grid,sin2T4_Grid); % set sterile parameters
+   
+   %%
+   D             = reshape(D,numel(D),1);
+   chi2Grid       = zeros(nGridSteps*nGridSteps,1);
+   FitResultsGrid = cell(nGridSteps*nGridSteps,1);
+   
+   parfor i= 1:(nGridSteps*nGridSteps)
+       D(i).SimulateStackRuns;
+       D(i).ModelObj.SetFitBiasSterile(mnu4Sq_Grid(i),sin2T4_Grid(i));
+       D(i).Fit
+       chi2Grid(i) = D(i).FitResult.chi2min;
+       FitResultsGrid{i} = D(i).FitResult;
+   end
+  
+  D              = reshape(D,nGridSteps,nGridSteps);
+  chi2Grid       = reshape(chi2Grid,nGridSteps,nGridSteps);
+  FitResultsGrid = reshape(FitResultsGrid,nGridSteps,nGridSteps);
     %%
-    chi2Grid        = arrayfun(@(x) x.FitResult.chi2min,D);
-    FitResultsGrid  = arrayfun(@(x) x.FitResult,D);
-    save(savefile,'chi2_ref','FitResults_ref','RunAnaArg',...
-        'chi2Grid','mnu4Sq','sin2T4','FitResultsGrid');
-    fprintf('save file to %s \n',savefile)
+%     chi2Grid        = arrayfun(@(x) x.FitResult.chi2min,D);
+%     FitResultsGrid  = arrayfun(@(x) x.FitResult,D);
+  % save(savefile,'chi2_ref','FitResults_ref','RunAnaArg',...
+  %     'chi2Grid','mnu4Sq','sin2T4','FitResultsGrid');
+  % fprintf('save file to %s \n',savefile)
 end
 
-%% get contour at X sigma
+% %% get contour at X sigma
 % CL = 90;
 % % Confidence level
 % switch CL
